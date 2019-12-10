0bc59d27adadfeccb56a5985449b87680ed3b087
Nikita Kazakov <djnikitos98@gmail.com>
2019-12-10 18:39:55 +0400

    Починил картинки. Правда хз как

--- src/main/java/com/railwaymodelingsystem/controller/HomeController.java
+++ src/main/java/com/railwaymodelingsystem/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.railwaymodelingsystem.controller;
 
+import com.railwaymodelingsystem.model.User;
+import com.railwaymodelingsystem.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+    final
+    UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/start_Constructor")
-    public String startConstructor(){
+    public String startConstructor(Model model, Principal principal){
+        User user = userService.getByName(principal.getName());
+        model.addAttribute("stations", user.getStations());
         return "startConstructor";
     }
 }
