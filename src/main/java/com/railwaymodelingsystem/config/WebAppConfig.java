f43a2b908d8db5a9f27de300581c1af864801c52
Nikita Kazakov <djnikitos98@gmail.com>
2019-12-10 18:07:17 +0400

    Починил картинки. Правда хз как

--- src/main/java/com/railwaymodelingsystem/config/WebAppConfig.java
+++ src/main/java/com/railwaymodelingsystem/config/WebAppConfig.java
@@ -1,10 +1,17 @@
 package com.railwaymodelingsystem.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebAppConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 }
